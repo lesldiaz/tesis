5b27cfbc020fc7a804409f909eecf9cde107f9d3
Pamela Pachacama <pamela_pachacama@hotmail.com>
2022-05-17 11:25:04 -0500

    formulario-registro
    registro-usuarios

--- frontend/tesis-proyecto/src/app/app.module.ts
+++ frontend/tesis-proyecto/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioRegistroComponent } from './componentes/formulario-registro/formulario-registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioRegistroComponent
   ],
   imports: [
     BrowserModule,
