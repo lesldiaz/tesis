528ee5fecc61a39c2382317d7723d16150d55d99
Pamela Pachacama <pamela_pachacama@hotmail.com>
2022-05-18 17:59:19 -0500

    notes
    postit-js-ts

--- frontend/tesis-proyecto/src/app/componentes/post-it/post-it.component.ts
+++ frontend/tesis-proyecto/src/app/componentes/post-it/post-it.component.ts
@@ -6,10 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-it.component.css']
 })
 export class PostItComponent implements OnInit {
+  [x: string]: any;
 
-  constructor() { }
+  constructor() {
+    }
 
   ngOnInit(): void {
-  }
 
+  }
+  //getNote().forEach(note=>{
+  // const noteElement = createNoteElement( note.id, note.content);
+  // notesContainer.insertBefore(noteElemnt, addNoteButton);
+  //
+  // });
+
+  getNotes(){
+    JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
+
+  }
+  //saveNotes(notes){
+    saveNotes(){
+   // localStorage.setItem("stickynotes-notes",JSON.stringify(notes));
+
+  }
+  //createNoteElement(id,content){
+    createNoteElement(){
+    const notesContainer = document.getElementById("app");
+    const addNoteButton = this['notesContainer'].querySelector(".add-note");
+    const element = document.createElement("textarea");
+    element.classList.add("note");
+    //element.value = content;
+    element.placeholder ="Empty sticky note";
+    element.addEventListener("change",()=> {
+      //this.updateNote(id,element.value);
+    });
+    element.addEventListener("dblclick",()=>{
+      const doDelete = confirm("Are you sure you wish to delete this sticky note?");
+      if(doDelete){
+       // this.deleteNote(id,element);
+      }
+    });
+    return element;
+  }
+  addNote(){
+
+  }
+  //updateNote(id, newContent){
+    updateNote(){
+    console.log("updating note ...");
+    //console.log(id,newContent);
+  }
+  //deleteNote(id,element){
+    deleteNote(){
+    console.log("delete note ...");
+    //console.log(id)26:34;
+  }
 }
