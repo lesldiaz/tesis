1f63b9947ea8eeb7c34cdffff621ff2ad1cd573c
Pamela Pachacama <pamela_pachacama@hotmail.com>
2022-05-19 15:48:18 -0500

    post-it
    funcionamiento de postit

--- frontend/tesis-proyecto/src/app/componentes/post-it/post-it.component.ts
+++ frontend/tesis-proyecto/src/app/componentes/post-it/post-it.component.ts
@@ -22,14 +22,11 @@ export class PostItComponent implements OnInit {
   porCadaUno(){
     this.notesContainer = document.getElementById("app");
     this.addNoteButton = this.notesContainer.querySelector(".add-note");
-    this.arrayNotes = this.getNotes();
+    this.arrayNotes = JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
     this.arrayNotes.forEach( (note: { id: number; content: string; }) => {
       const noteElement = this.createNoteElement( note.id, note.content);
       this.notesContainer.insertBefore(noteElement, this.addNoteButton);
     });
-  }
-  getNotes(){
-    JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
   }
   saveNotes(notes:object){
    // saveNotes(){
@@ -55,18 +52,20 @@ export class PostItComponent implements OnInit {
   addNote(){
     this.notesContainer = document.getElementById("app");
     this.addNoteButton = this.notesContainer.querySelector(".add-note");
-    this.existingNotes = this.getNotes();
+    this.existingNotes =JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
     const noteObject = {
       id: Math.floor(Math.random()*100000),
       content:""
     };
+    console.log(noteObject.id);
     const noteElement = this.createNoteElement(noteObject.id, noteObject.content);
     this.notesContainer.insertBefore(noteElement, this.addNoteButton);
     this.existingNotes.push(noteObject);
+
     this.saveNotes(this.existingNotes);
   }
   updateNote(id:number, newContent:string){
-    this.notes = this.getNotes();
+    this.notes = JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
     const targetNote = this.notes.filter((note: { id: number; }) => note.id == id)[0];
     targetNote.content = newContent;
     this.saveNotes(this.notes);
@@ -75,7 +74,7 @@ export class PostItComponent implements OnInit {
     console.log(id,newContent);
   }
   deleteNote(id:number,element:object){
-    this.notes = this.getNotes();
+    this.notes = JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");;
     const target = this.notes.filter((note: { id: number; }) => note.id != id);
     this.saveNotes(this.notes);
     this.notesContainer.removeChild(element);
