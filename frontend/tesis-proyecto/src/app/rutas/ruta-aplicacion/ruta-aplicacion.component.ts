d1a1a9e83e2b0c7992845398613d154448ab349d
Pamela Pachacama <pamela_pachacama@hotmail.com>
2022-05-19 16:45:13 -0500

    rutas-aplicacion
    rutas de aplicacion

--- frontend/tesis-proyecto/src/app/rutas/ruta-aplicacion/ruta-aplicacion.component.ts
+++ frontend/tesis-proyecto/src/app/rutas/ruta-aplicacion/ruta-aplicacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ruta-aplicacion',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutaAplicacionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly  _router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  irANuevo() {
+    const ruta = [ 'nuevoproyecto']
+    // /usuario/editar/1
+    this._router.navigate(ruta);
+  }
+  irAProyectos() {
+    const ruta = [ 'proyectos']
+    // /usuario/editar/1
+    this._router.navigate(ruta);
+  }
+  irAParticipantes() {
+    const ruta = [ 'participantes']
+    // /usuario/editar/1
+    this._router.navigate(ruta);
+  }
 }
