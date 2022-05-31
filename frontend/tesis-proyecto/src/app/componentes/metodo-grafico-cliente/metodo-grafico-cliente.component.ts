fe465e632537e1898fbfc318eb802b92115b1bc5
Pamela Pachacama <pamela_pachacama@hotmail.com>
2022-05-31 15:37:30 -0500

    rol
    crear elemento option con inputs

--- frontend/tesis-proyecto/src/app/componentes/metodo-grafico-cliente/metodo-grafico-cliente.component.ts
+++ frontend/tesis-proyecto/src/app/componentes/metodo-grafico-cliente/metodo-grafico-cliente.component.ts
@@ -7,12 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MetodoGraficoClienteComponent implements OnInit {
   roles:string[]=[];
+  rolesop:any;
+  container:any;
+  var = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
   cambioRol(){
     console.log('cambio')
+    this.container=document.getElementById('rol');
+    this.rolesop =document.getElementById('roles');
+    const option = document.createElement('option');
+    console.log(this.container.value);
+    this.var=0;
+    if(this.roles.length==0){
+      this.roles.push(this.container.value);
+      option.value=this.container.value;
+      this.rolesop.appendChild(option);
+    }else{
+      for (const role of this.roles) {
+        if(role == this.container.value){
+          this.var = this.var + 1;
+        }
+      }
+      console.log(this.var);
+        if(this.var == 0){
+          this.roles.push(this.container.value);
+          option.value=this.container.value;
+          this.rolesop.appendChild(option);
+        }
+    }
+    console.log(this.roles);
 
   }
+
 }
