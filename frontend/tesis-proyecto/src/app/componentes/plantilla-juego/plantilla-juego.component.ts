f620451e1d3e3d7a07d628b5911233cbd894a789
Leslie Diaz <lmdiaz36@gmail.com>
2022-09-05 22:42:02 -0500

    correccion error del error xd

--- frontend/tesis-proyecto/src/app/componentes/plantilla-juego/plantilla-juego.component.ts
+++ frontend/tesis-proyecto/src/app/componentes/plantilla-juego/plantilla-juego.component.ts
@@ -36,7 +36,7 @@ export class PlantillaJuegoComponent implements OnInit {
       const data = XLSX.utils.sheet_to_json(ws, {range: 5}); // to get 2d array pass 2nd parameter as object {header: 1}
       this.resultado = FUNCIONES_GENERALES.tratamientoDatosExcel(data as any);
       this.nroReqCargados = this.resultado.length;
-      this.requerimientosCargados.emit(this.resultado);
+      this.requerimientosCargadosJ.emit(this.resultado);
     }
     console.log("file juego");
   }
