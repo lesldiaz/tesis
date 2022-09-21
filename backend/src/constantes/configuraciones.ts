b4c715d575140967a22428b3eb316e2d4ef6dee4
Leslie Diaz <lmdiaz36@gmail.com>
2022-09-21 00:02:21 -0500

    correcciones config bdd

--- backend/src/constantes/configuraciones.ts
+++ backend/src/constantes/configuraciones.ts
@@ -1,14 +1,14 @@
 export const CONFIGURACIONES = {
         bdd: {
             type: 'mysql',
-            host: 'localhost', //vraw.cs5ithf0vstb.us-east-1.rds.amazonaws.com
-            port: 49153,
-            username: 'root',
-            password: 'mysqlpw',
-            database: 'vraw',
-            synchronize: true, // consumir la base, no crear --> false
-            dropSchema: false, // false para build
+            host: 'localhost', //url servidor bdd
+            port: 3306, // puerto servidor bdd
+            username: 'root', //credenciales bdd
+            password: 'root2022', //credenciales bdd
+            database: 'vraw', //nombre bdd
+            synchronize: false,
+            dropSchema: false,
         },
-        crearDatosTest: false// false para build
+        crearDatosTest: false
     }
 ;
